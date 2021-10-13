d96b105c15b2b031871ffd5ac2798792136f7f01
espen <EspenBG>
2021-10-13 14:45:14 +0200

    added classesnjbljk

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -3,14 +3,19 @@ import numpy as np
 
 class KalmanFilter:
     import numpy as np
-    """Kalman filter"""
-    def __init__(self, G, variance, H, start_value):
-        self.A_ = None  # Can not be made before delta_t is given 3x3 vector
+    """Kalman filter, 
+    """
+    def __init__(self, A, start_value, variance):
+        size = A.size()
+
+        self.A_ = A  # Can not be made before delta_t is given 3x3 vector
         self.x_priori_ = start_value  # Set initial start value 1x3 vector
 
-        self.H_ = H  # 3x1 vector
+        self.H_ = np.zeros(size[0])  # 3x1 vector
         self.x_Var_ = variance
         self.R_ = None
+
+        # Q matrix represent G*G^(-1)*(expected variance)
         self.Q_ = None
 
         self.x_ = 0
