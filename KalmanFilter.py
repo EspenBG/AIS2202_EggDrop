ed1b92f007af76b2fed8761f380991683d36ec62
espen <EspenBG>
2021-10-14 08:10:03 +0200

    changed init for KalmanFilter

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -5,7 +5,7 @@ class KalmanFilter:
     import numpy as np
     """Kalman filter, 
     """
-    def __init__(self, A, G, H, P, start_value, set_variance, white_variance = 0):
+    def __init__(self, A, G,  set_variance, white_variance=0, start_value=np.array([0, 0, 0]), H=None):
         size = A(0).size()
 
         self.A_ = A  # Can not be made before delta_t is given 3x3 vector
@@ -13,10 +13,14 @@ class KalmanFilter:
         self.x_priori_ = start_value  # Set initial start value 1x3 vector
         self.x_ = start_value
 
-        self.H_ = H  # 3x1 vector
+        if H is None:
+            self.H_ = H
+        else:
+            self.H_ = np.zeroes(size[0])  # 3x1 vector
+
         self.x_Var_ = set_variance
         self.R_ = white_variance
-        self.P_ = P
+        self.P_ = np.zeros(size)
         self.P_priori_ = None
         self.K_ = np.zeros(size[0])
 
@@ -40,7 +44,7 @@ class KalmanFilter:
         return self.x_.copy()
 
     def calc_Q_(self, delta_t):
-        self.Q_ = self.G_(delta_t)@np.transpose(self.G_(deta_t))*self.x_Var_
+        self.Q_ = self.G_(delta_t)@np.transpose(self.G_(delta_t))*self.x_Var_
 
     def predict(self, delta_t):
         self.x_priori_ = self.A_(delta_t)@self.x_ # + w_(k-1)  // dette er produkt stoy
